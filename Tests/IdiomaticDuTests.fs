a4e16b59e17dee0519bd7ddfbcebeb668d1455e8
Orlando William <0rlandw@gmail.com>
2015-02-15 20:52:14 -0500

    adding idiomatic du tests

---
+++ Tests/IdiomaticDuTests.fs
@@ -0,0 +1,21 @@
+﻿module IdiomaticDUTests
+
+open Tests
+open NUnit.Framework
+
+type Simple =
+    | Int of int
+    | String of string
+
+type Status =
+    | Named of active:bool * text:string
+    | Else of int
+
+[<Test>]
+let ``simple unnamed dus are serialized idiomatically`` () =
+    Int 5 |> serializes "{\"Case\":\"Int\",\"Fields\":[5]}"
+
+[<Test;Ignore>]
+let ``dus with named fields keep their names`` () =
+    Named (true, "lorem ipsum") 
+        |> serializes "{\"Case\":\"Named\",\"active\":true,\"text\":\"lorem ipsum\"}"
