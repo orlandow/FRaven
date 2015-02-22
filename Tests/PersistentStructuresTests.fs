bc0096313e0ef5d97f978f8d43da1af51af7659a
Orlando William <0rlandw@gmail.com>
2015-02-22 00:45:20 -0500

    added set converter

---
+++ Tests/PersistentStructuresTests.fs
@@ -0,0 +1,8 @@
+﻿module PersistentStructureTests
+
+open Tests
+open NUnit.Framework
+
+[<Test>]
+let ``can serialize a set``() =
+    same <| set [1;2;3;4]
--- Tests/Serializer.fs
+++ Tests/Serializer.fs
@@ -7,8 +7,9 @@ open Raven.Imports.Newtonsoft.Json
 let opt = new OptionConverter()
 let un = new UnionConverter()
 let li = new ListConverter()
+let set = new SetConverter()
 
-let converters : JsonConverter array = [| opt; un; li |]
+let converters : JsonConverter array = [| opt; un; li; set |]
 
 let serialize x =
     JsonConvert.SerializeObject(x, converters)
