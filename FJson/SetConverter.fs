42ccd731d19a54b895f5382e046f6570971a53ae
Orlando William <0rlandw@gmail.com>
2015-02-22 02:30:13 -0500

    set converter working

--- FJson/SetConverter.fs
+++ FJson/SetConverter.fs
@@ -5,23 +5,22 @@ open Microsoft.FSharp.Reflection
 open Raven.Imports.Newtonsoft.Json
 open System.Collections.Generic
 open Raven.Imports.Newtonsoft.Json.Linq
+open System.Reflection
 
 type SetConverter() =
     inherit JsonConverter()
 
-    let listConverter = new ListConverter()
-    
     override x.CanConvert(t:Type) = 
         t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<Set<_>>
  
     override x.WriteJson(writer, value, serializer) =
-        listConverter.WriteJson(writer, value, serializer)
+        let list = value :?> System.Collections.IEnumerable |> Seq.cast
+        serializer.Serialize(writer, list)
  
     override x.ReadJson(reader, t, obj, serializer) = 
         let itemType = t.GetGenericArguments().[0]
         let collectionType = typedefof<List<_>>.MakeGenericType(itemType)
         let collection = serializer.Deserialize(reader, collectionType) 
-                            :?> System.Collections.IEnumerable
-                            |> Seq.cast
 
-        set collection :> obj
+        let setType = typedefof<Set<_>>.MakeGenericType(itemType)
+        System.Activator.CreateInstance(setType, collection)
