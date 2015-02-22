8127c87f1db660f154fbb9f705c52adc4a0b8d34
Orlando William <0rlandw@gmail.com>
2015-02-21 20:32:36 -0500

    added write json and can convert

--- FJson/UnionConverter.fs
+++ FJson/UnionConverter.fs
@@ -3,12 +3,26 @@
 open System
 open Microsoft.FSharp.Reflection
 open Raven.Imports.Newtonsoft.Json
+open System.Collections
 
 type UnionConverter() =
     inherit JsonConverter()
 
-    override x.CanConvert(t) = false
+    let [<Literal>] CaseTag = "Case"
 
-    override x.WriteJson(writer, value, serializer) = ()
+    override x.CanConvert(t) = 
+        not(typeof<IEnumerable>.IsAssignableFrom(t)) && FSharpType.IsUnion(t)
+
+    override x.WriteJson(writer, value, serializer) = 
+        let t = value.GetType()
+        let case, fields = FSharpValue.GetUnionFields(value, t)
+
+        writer.WriteStartObject()
+        writer.WritePropertyName(CaseTag)
+        writer.WriteValue(case.Name)
+        fields |> Array.iteri (fun index value ->
+            writer.WritePropertyName(sprintf "Item%d" index)
+            serializer.Serialize(writer, value))
+        writer.WriteEndObject()
             
     override x.ReadJson(reader, t, existingValue, serializer) = obj()
