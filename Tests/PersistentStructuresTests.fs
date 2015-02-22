e1e1250ba24942b10add43020a837fca13f48df1
Orlando William <0rlandw@gmail.com>
2015-02-22 02:31:34 -0500

    more complex set tests

--- Tests/PersistentStructuresTests.fs
+++ Tests/PersistentStructuresTests.fs
@@ -3,9 +3,14 @@
 open Tests
 open NUnit.Framework
 
+type Enum = A | B | C
+
 [<Test>]
 let ``can serialize a set``() =
     same <| set [1;2;3;4]
+    same <| set [A; B; C]
+    same <| set [Some 3; None]
+    same <| set [set [1;2]; set [3;1]]
     
 [<Test>]
 let ``can serialize a map``() =
